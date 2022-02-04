4e3e4edcff7623345df6fa5c751efdedcaef5306
wasimclevator <wasim@clevator.com>
2022-02-04 22:55:55 +0530

    for testing

--- NavExample/app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ NavExample/app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -20,6 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
+        //wasim
         //hello comment
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.myapplication", appContext.getPackageName());
